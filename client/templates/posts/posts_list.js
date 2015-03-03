f423878917bf5447aeee4e27643134dd1689c0b7
Fernando Val <aaromnido@gmail.com>
2015-03-03 09:21:12 +0100

    Fixing a link issue

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,15 +1,15 @@
 var postsDatass = [
   {
     title: 'Introducing Telescope',
-    urls: 'http://sachagreif.com/introducing-telescope/'
+    url: 'http://sachagreif.com/introducing-telescope/'
   },
   {
     title: 'Meteor',
-    urls: 'http://meteor.com'
+    url: 'http://meteor.com'
   },
   {
     title: 'The Meteor Book',
-    urls: 'http://themeteorbook.com'
+    url: 'http://themeteorbook.com'
   }
 ];
 Template.postsList.helpers({
